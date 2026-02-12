968702307e6b76c2fe19d88e7193aa4e50003c70
Shivendrak maurya <shivendrakmaurya@gmail.com>
2026-02-12 22:26:39 +0530

    Mathmetics operations

---
+++ math_operation.js
@@ -0,0 +1,35 @@
+const readline =  require("readline");
+
+const prompt = readline.createInterface({
+               input: process.stdin,
+               output: process.stdout
+});
+
+prompt.question("Enter first number =", function(a){
+prompt.question("Enter second number = ", function(b){
+
+               a = parseFloat(a);
+               b= parseFloat(b);
+
+               let  sum = a + b;
+               let sub = a - b;
+               let mul = a * b;
+
+               console.log("Sum = ",sum);
+               console.log("Substraction =",sub);
+               console.log("Multiplication = ",mul);
+
+               if(b != 0){
+                              let div = a/b;
+                              console.log("Division = " + div);      
+               }else{
+                              console.log("Division is not possible (Dinominator is zero)");
+               }
+
+               prompt.close();
+});
+
+});
+
+
+

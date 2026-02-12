409038dc72287700139bbaba4aaed2aecdfa4aa7
Shivendrak maurya <shivendrakmaurya@gmail.com>
2026-02-12 22:34:59 +0530

    Mathmetics operations

--- math_operation.js
+++ math_operation.js
@@ -9,21 +9,21 @@ prompt.question("Enter first number =", function(a){
 prompt.question("Enter second number = ", function(b){
 
                a = parseFloat(a);
-               b= parseFloat(b);
+               b = parseFloat(b);
 
                let  sum = a + b;
                let sub = a - b;
                let mul = a * b;
 
                console.log("Sum = ",sum);
-               console.log("Substraction =",sub);
+               console.log("Subtraction =",sub);
                console.log("Multiplication = ",mul);
 
-               if(b != 0){
+               if(b !== 0){
                               let div = a/b;
                               console.log("Division = " + div);      
                }else{
-                              console.log("Division is not possible (Dinominator is zero)");
+                              console.log("Division is not possible (Denominator is zero)");
                }
 
                prompt.close();

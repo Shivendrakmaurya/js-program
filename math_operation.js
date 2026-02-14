7e6c34b0944d789921f8971220f7b63355734b41
Shivendrak maurya <shivendrakmaurya@gmail.com>
2026-02-14 23:57:42 +0530

    Mathematics operations

--- math_operation.js
+++ math_operation.js
@@ -1,33 +1,33 @@
-const readline =  require("readline");
+const readline = require("readline");
 
 const prompt = readline.createInterface({
                input: process.stdin,
                output: process.stdout
 });
 
-prompt.question("Enter first number =", function(a){
-prompt.question("Enter second number = ", function(b){
+prompt.question("Enter first number =", function (a) {
+               prompt.question("Enter second number = ", function (b) {
 
-               a = parseFloat(a);
-               b = parseFloat(b);
+                              a = parseFloat(a);
+                              b = parseFloat(b);
 
-               let  sum = a + b;
-               let sub = a - b;
-               let mul = a * b;
+                              let sum = a + b;
+                              let sub = a - b;
+                              let mul = a * b;
 
-               console.log("Sum = ",sum);
-               console.log("Subtraction =",sub);
-               console.log("Multiplication = ",mul);
+                              console.log("Sum = ", sum);
+                              console.log("Subtraction =", sub);
+                              console.log("Multiplication = ", mul);
 
-               if(b !== 0){
-                              let div = a/b;
-                              console.log("Division = " + div);      
-               }else{
-                              console.log("Division is not possible (Denominator is zero)");
-               }
+                              if (b !== 0) {
+                                             let div = a / b;
+                                             console.log("Division = " + div);
+                              } else {
+                                             console.log("Division is not possible (Denominator is zero)");
+                              }
 
-               prompt.close();
-});
+                              prompt.close();
+               });
 
 });
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,35 @@
+
+// Program to find the largest of three numbers
+
+
+// Importing readline module to take input from user
+const readline = require("readline");
+
+const prompt = readline.createInterface({
+               input: process.stdin,
+               output: process.stdout
+
+});
+
+// Taking input from user
+prompt.question("Enter first number = ", function (a) {
+               prompt.question("Enter second number = ", function (b) {
+                              prompt.question("Enter third number = ", function (c) {
+                                             a = parseInt(a);
+                                             b = parseInt(b);
+                                             c = parseInt(c);
+
+                                             // Finding the largest number among a, b and c
+                                             if (a >= b && a >= c) {
+                                                            console.log("Largest number is = " + a);
+                                             }
+                                             else if (b >= a && b >= c) {
+                                                            console.log("Largest number is = " + b);
+                                             }
+                                             else {
+                                                            console.log("Largest number is = " + c);
+                                             }
+                                             prompt.close();
+                              });
+               });
+});
